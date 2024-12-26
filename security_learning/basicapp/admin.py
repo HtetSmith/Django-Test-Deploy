87a82642eaf426dee65f8f62cbb3168db966a1f6
HtetSmith <107472988+HtetSmith@users.noreply.github.com>
2024-12-26 10:23:57 +0630

    first django commit

---
+++ security_learning/basicapp/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from basicapp.models import UserProfileInfo
+# Register your models here.
+admin.site.register(UserProfileInfo)
+
+#super_user_acc  username = james , ps = warhammer40k@ YoYo12345@
